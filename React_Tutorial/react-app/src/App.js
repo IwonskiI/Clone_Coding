3e3413ce0c6fa76cd729857637e484deb1636a63
IwonskiI <ricky2101@naver.com>
2023-11-03 15:42:32 +0900

    React Complete

--- React_Tutorial/react-app/src/App.js
+++ React_Tutorial/react-app/src/App.js
@@ -49,6 +49,29 @@ function Create(props){
   </article>
 }
 
+function Update(props){
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+
+  return <article>
+    <h2>Update</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onUpdate(title, body)
+    }}>
+      <p><input type='text' name="title" placeholder='title' value={title} onChange={event=>{
+        setTitle(event.target.value);
+      }}></input></p>
+      <p><textarea name='body' placeholder='body' value={body} onChange={event=>{
+        setBody(event.tartget.value);
+      }}></textarea></p>
+      <p><input type='submit' value='Update'></input></p>
+    </form>
+  </article>
+}
+
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
@@ -59,6 +82,7 @@ function App() {
     {id:3, title:'js', body:'js is ...'}
   ]);
   let content = null;
+  let contextControl = null;
   if(mode === 'WELCOME'){
     content = <Article title="Welcome" body="Hello,WEB"></Article>
   } 
@@ -66,12 +90,28 @@ function App() {
     let title, body = null;
     for(let i =0; i<topics.length; i++){
       if(topics[i].id === Number(id)){
-        title = topics[i].title
-        body = topics[i].body
+        title = topics[i].title;
+        body = topics[i].body;
         break
       }
     }
-    content = <Article title={title} body={body}></Article>
+    content = <Article title={title} body={body}> </Article>
+    contextControl = <>
+      <li><a href={'/update/'+id} onClick={event=>{
+        event.preventDefault();
+        setMode('UPDATE');
+      }}>Update</a></li>
+      <li><input type='button' value="Delete" onClick={()=>{
+        const newTopics = [];
+        for(let i=0; i<topics.length; i++){
+          if(topics[i].id !== id){
+            newTopics.push(topics[i]);
+          }
+        }
+        setTopics(newTopics);
+        setMode('WELCOME');
+      }}></input></li>
+    </>
   }
   else if(mode === 'CREATE'){
     content = <Create onCreate={(_title, _body)=>{
@@ -84,16 +124,40 @@ function App() {
       setMode('READ');
     }}></Create>
   }
+  else if(mode === 'UPDATE'){
+    let title, body = null;
+    for(let i =0; i<topics.length; i++){
+      if(topics[i].id === Number(id)){
+        title = topics[i].title;
+        body = topics[i].body;
+        break
+      }
+    }
+    content = <Update title={title} body={body} onUpdate={(title, body)=>{
+      const newTopics = [...topics];
+      const updatedTopic = {id:id, title:title, body:body};
+      for(let i=0; i<newTopics.length; i++){
+        if(newTopics[i].id === id){
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode('READ');
+    }}></Update>
+  }
   return (
     <div>
       <Header title="WEB" onchange={()=>{setMode('WELCOME');}}></Header>
       <Nav topics = {topics} onchange={(id)=>{setMode('READ'); setId(id);}}></Nav>
       {content}
-      <a href="/create" onClick={event=>{
-        event.preventDefault();
-        setMode('CREATE');
-      }
-      }>Create</a>
+      <ul>
+        <li><a href="/create" onClick={event=>{
+          event.preventDefault();
+          setMode('CREATE');
+        }}>Create</a></li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
